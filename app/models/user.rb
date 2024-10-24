c5dc49f66e2025fbb322dd8cf5ecf9a89a876b09
peter0726 <jiuyuanqianming@gmail.com>
2024-10-24 20:40:57 +0900

    validates 追加
    ログインできない問題の解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :last_name_kanji, presence: true
+         validates :first_name_kanji, presence: true
+         validates :last_name_kana, presence: true
+         validates :first_name_kana, presence: true
+         validates :birth_day, presence: true
+
+
+         validate :password_complexity
+
+         private
+
+         def password_complexity
+           if password.present? && !password.match(/^(?=.*[a-zA-Z])(?=.*[0-9])/)
+             errors.add :password, "は英字と数字の両方を含める必要があります"
+           end
+         end
+
 end
