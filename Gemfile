36c15c7fd8b5db53ecbb56a474815e454454d906
peter0726 <jiuyuanqianming@gmail.com>
2024-10-24 18:28:29 +0900

    deviseの設定
    会員登録、ログイン、ログアウトのページ遷移を含む。

--- Gemfile
+++ Gemfile
@@ -82,3 +82,5 @@ group :production do
 end
 
 gem 'active_hash'
+
+gem 'devise'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   def index
@@ -14,7 +15,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
-
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name_kanji, :first_name_kanji, :last_name_kana, :first_name_kana, :birth_day ])
+  end
 
 
   
