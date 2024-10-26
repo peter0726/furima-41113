58b41150480fcd3450f1956289f76fe106c8cb04
peter0726 <jiuyuanqianming@gmail.com>
2024-10-26 11:27:25 +0900

    出品のボタンから出品画面への遷移
    ログインしているユーザーはそのまま出品画面へ遷移し、ログアウト状態のユーザーはログイン画面に遷移するようにした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
+
+
   def index
+    @items = Item.all
+
   end
+
+  def new
+
+  end
+
+
+  private
+
+  def message_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   root to: "items#index"
 
   resources :users
+  resources :items
 
 
 
