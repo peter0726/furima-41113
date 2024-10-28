331e3af2dcf5e42dd9a391173504980ff4563d59
peter0726 <jiuyuanqianming@gmail.com>
2024-10-28 21:08:37 +0900

    authenticate_user!にdestroyを追加した

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, except: [:index, :new, :create]
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :move_to_index, except: [:index, :show, :new, :create]
 
   def index
