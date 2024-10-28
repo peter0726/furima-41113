6f587b9cbcee48aaeaebcf986e627ff8b6f3bc41
peter0726 <jiuyuanqianming@gmail.com>
2024-10-28 15:50:08 +0900

    rubocop導入

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Item, type: :model do
       it 'ユーザー情報がない場合は登録できないこと' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include "User must exist"
+        expect(@item.errors.full_messages).to include 'User must exist'
       end
       it 'imageが空では登録できない' do
         @item.image = nil
