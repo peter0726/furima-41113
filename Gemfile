125da826ce15f9553e7b4f4eda8cc73bfb01eec0
peter0726 <jiuyuanqianming@gmail.com>
2024-10-29 15:00:58 +0900

    payjp導入

--- Gemfile
+++ Gemfile
@@ -91,3 +91,5 @@ gem 'devise'
 
 gem 'mini_magick'
 gem 'image_processing', '~> 1.2'
+
+gem 'payjp'
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,12 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order = Order.new(order_params)
     if @order.valid?
+      Payjp.api_key = "sk_test_***********"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: order_params[:price],  # 商品の値段
+        card: order_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
       @order.save
       return redirect_to root_path
     else
