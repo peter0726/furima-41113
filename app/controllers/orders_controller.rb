4da51564ab337e9545c55a65309f254ccd293c25
peter0726 <jiuyuanqianming@gmail.com>
2024-10-29 13:20:39 +0900

    orderがデータベースに保存されるようにした

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,14 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index', status: :unprocessable_entity
+    end
   end
 
 
