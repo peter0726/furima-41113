ff42b9c7794c7f54e131a849ccc3ed1260361469
peter0726 <jiuyuanqianming@gmail.com>
2024-10-29 16:03:43 +0900

    公開鍵と秘密鍵を見えないようにしたの追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,6 +14,7 @@ class OrdersController < ApplicationController
       @order.save
       return redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render 'index', status: :unprocessable_entity
     end
   end
